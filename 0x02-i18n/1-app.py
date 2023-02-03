f5881e8426377c0bd422f0462119b6d9d4860305
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-04 00:59:45 +0100

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template. request
 from flask_babel import Babel
 
 
-class Config():
+class Config(object):
     """Class that helps configure babel"""
     LANGUAGES = ['en', 'fr']
     TIMEZONE = 'UTC'
@@ -18,13 +18,13 @@ babel = Babel(app)
 @app.route('/')
 def index():
     """Render Home page """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 @babel.localeselector
 def get_locale():
     """Get the locale for configuration """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'][0])
 
 
 @babel.timezoneselector
