e5bbb34c49b8a54354f9a1bc3f13df9bf599eeaa
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-04 01:23:35 +0100

    task 2

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Contains the Flask application app """
-from flask import Flask, render_template. request
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
