9daa48e03601b647a9cb7f2e233ecc5b578f7a48
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-04 01:30:38 +0100

    done

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -27,6 +27,7 @@ def get_locale():
     """Get the locale for configuration"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 """
 @babel.timezoneselector
 def get_timezone():Get the timezone for configuration
