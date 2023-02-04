0647195e5063a97926859e402916858394213e7a
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-04 02:21:34 +0100

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Contains the Flask application app """
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config(object):
+    """Class that helps configure babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """Render Home page """
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Get the locale for configuration"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+"""
+@babel.timezoneselector
+def get_timezone():Get the timezone for configuration
+    return app.config['BABEL_DEFAULT_TIMEZONE']
+"""
+
+if __name__ == '__main__':
+    app.run(debug=True)
