2b1678e97e58c6f1ceae52a8d1695ff5d54040cc
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-05 22:22:08 +0100

    task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Contains the Flask application app """
+from flask import Flask, render_template, request
+from flask_babel import Babel as Bab
+
+
+class Config(object):
+    """Class that helps configure babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """Render Home page for the application """
+    return render_template('3-index.html')
+
+
+# @babel.localeselector
+def get_locale():
+    """Get the locale for configuration"""
+    locale = request.args.get('locale')
+    if locale:
+        return locale
+    # return request.accept_languages.best_match(['fr'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel = Bab(app, locale_selector=get_locale)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
