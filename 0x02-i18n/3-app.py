8f4cef02e09f564c3671acec84339cb8d3ffc4d8
Abdulmuizz Hamzat <hamzatabdulmuizz98@gmail.com>
2023-02-05 21:18:16 +0100

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Contains the Flask application app """
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel as Bab
 
 
 class Config(object):
@@ -13,7 +13,6 @@ class Config(object):
 
 app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app)
 
 
 @app.route('/')
@@ -22,12 +21,13 @@ def index():
     return render_template('3-index.html')
 
 
-@babel.localeselector
+# @babel.localeselector
 def get_locale():
     """Get the locale for configuration"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(['fr'])
+    # return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-
+babel = Bab(app, locale_selector=get_locale)
 """
 @babel.timezoneselector
 def get_timezone():Get the timezone for configuration
